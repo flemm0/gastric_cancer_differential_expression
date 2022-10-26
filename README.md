953235a6e9573a27af85a90b886db5fc276c4bd0
Flemming <flemming.wu@gmail.com>
2022-10-25 18:46:11 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repository holds the work done for the final project for TRGN 514.
---
+++ server-commands.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# make directories
+
+cd /scratch/
+mkdir FASTQS REFS Tophat-BAMS Cuffdiff-STATS Cuffdiff-noSTATS
+
+
+# download SRA sequences as fastq files
+
+cd FASTQS
+
+for (( i = 70; i <= 77; i++ ))
+do
+	nohup fasterq-dump -S SRR5855$i
+done
+
+# check read counts
+
+for (( i = 70; i <= 77; i++ ))
+do
+	wc -l SRR5855${i}_1.fastq | awk '{x=$1/4; print x}'
+	wc -l SRR5855${i}_2.fastq | awk '{x=$1/4; print x}'
+done
