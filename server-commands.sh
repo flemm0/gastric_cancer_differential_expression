dabe4bffde39a6b5e70a50ca8d3e69694f0e3bdc
Flemming <flemming.wu@gmail.com>
2022-10-27 22:23:42 -0700

    check that paired end reads are not the same

--- server-commands.sh
+++ server-commands.sh
@@ -24,3 +24,15 @@ do
 	echo "Reads for SRR5855${i}_1:"
 	wc -l SRR5855${i}_2.fastq | awk '{x=$1/4; print x}'
 done
+
+
+# check that read 1 and read 2 are not the same
+
+for (( i = 70; i <= 77; i++ ))
+do
+	echo "SRR5855${i}_1:"
+	head -n2 SRR5855${i}_1.fastq
+	echo "SRR5855${i}_2:"
+	head -n2 SRR5855${i}_1.fastq
+	printf '\n'
+done
