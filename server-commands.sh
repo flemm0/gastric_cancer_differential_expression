f6b64bb4d33988f6b8bd0a70aa3c86e20337ec21
Flemming <flemming.wu@gmail.com>
2022-11-02 11:58:28 -0700

    download ref genome fa and gtf file

--- server-commands.sh
+++ server-commands.sh
@@ -44,3 +44,15 @@ done
 
 ### server cpu architecture is x86_64
 
+
+######
+
+# download human reference genome fasta and gtf files from ensembl
+
+cd ../REFS
+
+nohup wget https://ftp.ensembl.org/pub/current_fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz &
+
+nohup wget https://ftp.ensembl.org/pub/current_gtf/homo_sapiens/Homo_sapiens.GRCh38.108.chr.gtf.gz &
+
+gunzip *.gz
